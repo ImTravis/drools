cde2f3594946b61132ddbed8367dc8b5b9025ad9
1139539742@qq.com <1139539742@qq.com>
2019-12-13 17:19:11 +0800

    add group  activation-group and agenda-group

--- src/test/java/com/drools/rules/ruleTest/StudentAgeRuleTest.java
+++ src/test/java/com/drools/rules/ruleTest/StudentAgeRuleTest.java
@@ -2,6 +2,7 @@ package com.drools.rules.ruleTest;
 
 import com.drools.rules.entity.StudentFact;
 import org.drools.core.base.RuleNameEndsWithAgendaFilter;
+import org.drools.core.base.RuleNameStartsWithAgendaFilter;
 import org.junit.jupiter.api.Test;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ public class StudentAgeRuleTest {
     @Autowired
     private KieSession kieSession;
 
+    /**
+     * Hello word
+     */
     @Test
     public void checkAge(){
         StudentFact studentFact = new StudentFact();
@@ -22,4 +26,42 @@ public class StudentAgeRuleTest {
         int sum =  kieSession.fireAllRules(new RuleNameEndsWithAgendaFilter("checkAge"));
         System.out.print("sum:"+sum);
     }
+
+
+
+    /**
+     * 技术点：activation-group
+     * 对低于70、60分的同学进行处理
+     */
+    @Test
+    public void gradeLow(){
+        StudentFact studentFact = new StudentFact();
+        studentFact.setAge(19);
+        studentFact.setGrade(59);
+        studentFact.setName("莉莉");
+        kieSession.insert(studentFact);
+
+        int sum =  kieSession.fireAllRules(new RuleNameStartsWithAgendaFilter("gradeLow"));
+        System.out.print("sum:"+sum);
+    }
+
+
+
+    /**
+     * 技术点：agenda-group
+     * 对低于70、60分的同学进行处理
+     */
+    @Test
+    public void gradeLow_aAgendaGroup(){
+        StudentFact studentFact = new StudentFact();
+        studentFact.setAge(19);
+        studentFact.setGrade(59);
+        studentFact.setName("莉莉");
+        kieSession.getAgenda().getAgendaGroup("gradeLow").setFocus();
+        kieSession.insert(studentFact);
+        int sum =  kieSession.fireAllRules(new RuleNameEndsWithAgendaFilter("gradeLowSixty_aAgendaGroup"));
+        System.out.print("sum:"+sum);
+    }
+
+
 }
